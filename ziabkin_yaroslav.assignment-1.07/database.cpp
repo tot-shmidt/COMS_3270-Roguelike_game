53220da19d22a04cc7a627b7cbec320834a198b4
Yar Neprost <yarik@iastate.edu>
2026-04-07 23:38:07 -0500

    Parsed moves.csv and pokemon.csv

--- ziabkin_yaroslav.assignment-1.07/database.cpp
+++ ziabkin_yaroslav.assignment-1.07/database.cpp
@@ -1,14 +1,23 @@
 #include "database.h"
 #include <iostream>
 #include <sstream>
+#include <limits.h>
 
 
 // ~~~ PARSING FUNCTIONS ~~~
+int atoi_check(std::string *str) {
+    if ((*str).empty()) {
+        return INT_MAX;
+    } else {
+        return std::stoi(*str);
+    }
+}
+
 std::vector<pokemon_db> parse_pokemon(std::ifstream* csv_file) {
     // Vector to store pokemon_db objects
     std::vector<pokemon_db> pokemon_db_objects = std::vector<pokemon_db>();
 
-    if (csv_file == NULL) { return pokemon_db_objects; }
+    if (csv_file == NULL) {return pokemon_db_objects;}
 
     // String to read 1 record from .csv file
     std::string record = std::string();
@@ -76,7 +85,7 @@ std::vector<pokemon_db> parse_pokemon(std::ifstream* csv_file) {
         std::getline(string_stream, entry, ',');
         db_object.is_default = std::stoi(entry);
 
-        // Push final db_object to the back of the
+        // Push final db_object to the back of the vector
         pokemon_db_objects.push_back(db_object);
     }
 
@@ -84,7 +93,131 @@ std::vector<pokemon_db> parse_pokemon(std::ifstream* csv_file) {
 }
 
 std::vector<moves_db> parse_moves(std::ifstream* csv_file) {
-    std::vector<moves_db> ;
+    std::vector<moves_db> moves_db_objects = std::vector<moves_db>();
+
+    if (csv_file == NULL) {return moves_db_objects;}
+
+    std::string record = std::string();
+
+    std::getline(*csv_file, record);
+
+    while (std::getline(*csv_file, record)) {
+        moves_db db_object = moves_db();
+
+        std::stringstream string_stream = std::stringstream(record);
+        std::string entry = std::string();
+
+        // ####################
+        // #      1.id        #
+        // ####################
+        std::getline(string_stream, entry, ',');
+        db_object.id = std::stoi(entry);
+
+        // ####################
+        // #   2.identifier   #
+        // ####################
+        std::getline(string_stream, entry, ',');
+        db_object.identifier = entry;
+
+        // ####################
+        // # 3.generation_id  #
+        // ####################
+        std::getline(string_stream, entry, ',');
+        if (entry.empty()) {
+            db_object.generation_id = INT_MAX;
+        } else {
+            db_object.generation_id = std::stoi(entry);
+        }
+
+        // ####################
+        // #     4.type_id    #
+        // ####################
+        std::getline(string_stream, entry, ',');
+        db_object.type_id = atoi_check(&entry);
+
+        // ####################
+        // #     5.power      #
+        // ####################
+        std::getline(string_stream, entry, ',');
+        if (entry.empty()) {
+            db_object.power = INT_MAX;
+        } else {
+            db_object.power = std::stoi(entry);
+        }
+
+        // #####################
+        // #       6.pp        #
+        // #####################
+        std::getline(string_stream,entry, ',');
+        db_object.pp = atoi_check(&entry);
+
+        // ####################
+        // #     7.accuracy   #
+        // ####################
+        std::getline(string_stream, entry, ',');
+        if (entry.empty()) {
+            db_object.accuracy = INT_MAX;
+        } else {
+            db_object.accuracy = std::stoi(entry);
+        }
+
+        // ####################
+        // #    8.priority    #
+        // ####################
+        std::getline(string_stream, entry, ',');
+        db_object.priority = atoi_check(&entry);
+
+        // #####################
+        // #   9.target_id     #
+        // #####################
+        std::getline(string_stream,entry, ',');
+        db_object.target_id = atoi_check(&entry);
+
+        // #######################
+        // #  10.damage_class_id #
+        // #######################
+        std::getline(string_stream, entry, ',');
+        db_object.damage_class_id = atoi_check(&entry);
+
+        // ####################
+        // #    11.effect_id  #
+        // ####################
+        std::getline(string_stream, entry, ',');
+        db_object.effect_id = atoi_check(&entry);
+
+        // #####################
+        // #  12.effect_chance #
+        // #####################
+        std::getline(string_stream,entry, ',');
+        if (entry.empty()) {
+            db_object.effect_chance = INT_MAX;
+        } else {
+            db_object.effect_chance = std::stoi(entry);
+        }
+
+        // #######################
+        // #  13.contest_type_id #
+        // #######################
+        std::getline(string_stream, entry, ',');
+        db_object.contest_type_id = atoi_check(&entry);
+
+        // ##########################
+        // #  14.contest_effect_id  #
+        // ##########################
+        std::getline(string_stream, entry, ',');
+        db_object.contest_effect_id = atoi_check(&entry);
+
+        // ################################
+        // #  15.super_contest_effect_id  #
+        // ################################
+        std::getline(string_stream, entry, ',');
+        db_object.super_contest_effect_id = atoi_check(&entry);
+
+        // Push final db_object to the back of the vector
+        moves_db_objects.push_back(db_object);
+    }
+
+    return moves_db_objects;
 }
 
 // ~~~ PRINTING FUNCTIONS ~~~
@@ -95,5 +228,20 @@ void print_pokemon_db(std::vector<pokemon_db>* db_vector) {
 }
 
 void print_moves_db(std::vector<moves_db>* db_vector) {
-
+    for (moves_db obj : *db_vector) {
+        std::cout << obj.id << "," << obj.identifier << ",";
+        std::cout << (obj.generation_id == INT_MAX ? "" : std::to_string(obj.generation_id)) << ",";
+        std::cout << (obj.type_id == INT_MAX ? "" : std::to_string(obj.type_id)) << ",";
+        std::cout << (obj.power == INT_MAX ? "" : std::to_string(obj.power)) << ",";
+        std::cout << (obj.pp == INT_MAX ? "" : std::to_string(obj.pp)) << ",";
+        std::cout << (obj.accuracy == INT_MAX ? "" : std::to_string(obj.accuracy)) << ",";
+        std::cout << (obj.priority == INT_MAX ? "" : std::to_string(obj.priority)) << ",";
+        std::cout << (obj.target_id == INT_MAX ? "" : std::to_string(obj.target_id)) << ",";
+        std::cout << (obj.damage_class_id == INT_MAX ? "" : std::to_string(obj.damage_class_id)) << ",";
+        std::cout << (obj.effect_id == INT_MAX ? "" : std::to_string(obj.effect_id)) << ",";
+        std::cout << (obj.effect_chance == INT_MAX ? "" : std::to_string(obj.effect_chance)) << ",";
+        std::cout << (obj.contest_type_id == INT_MAX ? "" : std::to_string(obj.contest_type_id)) << ",";
+        std::cout << (obj.contest_effect_id == INT_MAX ? "" : std::to_string(obj.contest_effect_id)) << ",";
+        std::cout << (obj.super_contest_effect_id == INT_MAX ? "" : std::to_string(obj.super_contest_effect_id)) << std::endl;
+    }
 }
